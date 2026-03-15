f1a25a69f7007a5fbc03cde640ad40fc83b989e3
wishes-marker <wangshanhua54@qq.com>
2026-03-15 11:56:19 +0800

    go定义变量

--- main.go
+++ main.go
@@ -9,10 +9,20 @@ func main()  {
     fmt.Println(i)
 	fmt.Printf("%v\n",i)
 
+	
 	var a int=10
 	var b int=3
 	var c int=5
     fmt.Println("a=",a,"b=",b,"c=",c)
 	fmt.Printf("a=%v b=%v c=%v",a,b,c)
+
+//类型推导方式定义变量
+	d :=10
+	e :=20
+	f :=30
+    fmt.Printf("d=%v e=%v f=%v \n",d,e,f)
+	//使用Printf打印一个变量的类型
+	fmt.Printf("d=%v d的类型是%T",d,d)
+    
 
 }
